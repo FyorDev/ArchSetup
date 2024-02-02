dbe4c787f993127dbebeb178f521b9a8c019fd3a
FyorUU <uuf@fyor.nl>
2024-02-02 01:12:29 +0100

    Fix wine multilib installation

--- scripts/setup/root_wine.sh
+++ scripts/setup/root_wine.sh
@@ -5,7 +5,9 @@ current_user=$(logname)
 
 # Enable multilib which is required for the wine repository
 sudo sed -i ':a;N;$!ba;s@#\[multilib\]\n#Include = \/etc\/pacman.d\/mirrorlist\n@\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist\n@' /etc/pacman.conf
-# TODO: install multilib lol
+
+pacman -Syu # install newly added multilib
+
 packages=(
   wine
   wine-mono
