035fb3bee5e4bf73a75d64914e943c40aecaa6f1
FyorDev <uuf@fyor.nl>
2024-02-06 23:00:11 +0000

    Deploy Blender and Godot theme

--- scripts/software/root_media.sh
+++ scripts/software/root_media.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 current_user=$(logname)
+user_home=$(eval echo ~$SUDO_USER)
 
 packages=(
   obsidian
@@ -16,4 +17,9 @@ aurpackages=(
   xp-pen-tablet
 )
 sudo -u $current_user yay -S "${aurpackages[@]}" --noconfirm
+
+mkdir -p $user_home/.config/godot/text_editor_themes
+cp ./dotfiles/godot/catppuccin_mocha.tet $user_home/.config/godot/text_editor_themes/
+
+cp ./dotfiles/blender/mocha_mauve.xml $user_home/.config/blender/*/scripts/presets/interface_theme/
 
