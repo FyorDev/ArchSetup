46cdac0e57ebf213c0a1521c8348f44c5510703b
FyorDev <uuf@fyor.nl>
2024-02-10 18:56:42 +0100

    Davinci AUR currently broken for newest kernel

--- scripts/software/root_media.sh
+++ scripts/software/root_media.sh
@@ -13,7 +13,6 @@ packages=(
 pacman -S "${packages[@]}" --noconfirm
 
 aurpackages=(
-  davinci-resolve # add davinconv for supported formats?
   xp-pen-tablet
 )
 sudo -u $current_user yay -S "${aurpackages[@]}" --noconfirm
